d3499888b5ef9bf694d57661f1cd7100d08da977
Artem Chabanniy <artem.chabanniy@gmail.com>
2015-02-09 16:19:34 +0200

    Added mac project.

---
+++ IDPMailView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IDPMailView
+//
+//  Created by Artem Chabanniy on 2/9/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IDPMailView/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  IDPMailView
+//
+//  Created by Artem Chabanniy on 2/9/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
