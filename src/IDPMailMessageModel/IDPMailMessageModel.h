aadce4f21eb41186bf9627904134c71e8109f18a
Artem Chabanniy <artem.chabanniy@gmail.com>
2015-02-10 16:13:38 +0200

    Added mail message model.

---
+++ src/IDPMailMessageModel/IDPMailMessageModel.h
@@ -0,0 +1,20 @@
+//
+//  IDPMailMessageModel.h
+//  IDPMailView
+//
+//  Created by Artem Chabanniy on 2/10/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IDPMailMessageModel : NSObject
+
+@property (nonatomic, strong) NSArray   *sender;
+@property (nonatomic, strong) NSArray   *recipients;
+@property (nonatomic, copy)   NSString  *subject;
+@property (nonatomic, assign, getter = isRead) BOOL read;
+@property (nonatomic, strong) NSDate    *date;
+@property (nonatomic, copy)   NSString  *text;
+ 
+@end
---
+++ src/IDPMailMessageModel/IDPMailMessageModel.m
@@ -0,0 +1,15 @@
+//
+//  IDPMailMessageModel.m
+//  IDPMailView
+//
+//  Created by Artem Chabanniy on 2/10/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import "IDPMailMessageModel.h"
+
+@implementation IDPMailMessageModel
+
+
+
+@end
