6c85164d565e723f62fbce9410c94729b3110b42
Artem Chabanniy <artem.chabanniy@gmail.com>
2015-02-17 17:39:42 +0200

    Added view and view controller.

---
+++ src/IDPMailPreviewViewController/IDPMailPreviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDPMailPreviewViewController.h
+//  IDPMailView
+//
+//  Created by Artem Chabanniy on 2/17/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IDPMailPreviewViewController : NSViewController
+
+@end
---
+++ src/IDPMailPreviewViewController/IDPMailPreviewViewController.m
@@ -0,0 +1,22 @@
+//
+//  IDPMailPreviewViewController.m
+//  IDPMailView
+//
+//  Created by Artem Chabanniy on 2/17/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import "IDPMailPreviewViewController.h"
+
+@interface IDPMailPreviewViewController ()
+
+@end
+
+@implementation IDPMailPreviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
